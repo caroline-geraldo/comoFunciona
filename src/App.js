baffc408c54d183805b34f9c9d869f70ba877120
Caroline geraldo <carolinegeraldo@yahoo.com.br>
2020-05-26 11:55:03 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,59 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="section__comoFunciona">
+      <div className="section__comoFuncionaTitle">
+      <h1>Como funciona?</h1>
+      <p>  Desenvolva sua ideia e o seu site com a gente e aproveite! </p>
+
+      </div>
+      <section className="section__comoFunciona_content">
+
+      <section class="container">
+	
+  <div class="box">
+      
+      <h1>Tem um projeto?</h1>
+     
+  </div>
+
+  <div class="box">
+     
+  </div>
+
+  <div class="box">
+      
+  </div>
+
+  <div class="box">
+      <h1>Defina</h1>
+      
+      
+  </div>
+
+  <div class="box">
+      <h1>Defina</h1>
+     
+  </div>
+
+  
+       
+</section>
+<div>
+
+</div>
+
+
+      </section>
+    
+
     </div>
+    
+
+
+    </>
+   
   );
 }
 
