951dbc6dd6b764f439382458bc941460f7772134
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2020-05-27 11:44:57 -0300

    Add code showed in class

--- src/App.js
+++ src/App.js
@@ -1,76 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import CreativeThinkingUrl from './assets/img/creative-thinking.png'
+import EscolharOMelhorUrl from './assets/img/escolhaOMelhor.jpg'
+import Box from './components/Box';
+
+import Acordeao from './components/Acordeao';
 
 function App() {
+
+  const [formName, setFormName] = useState('');
+  const [formPassword, setFormPassword] = useState('');
+
+  const handleName = (event) => {
+    setFormName(event.target.value.toUpperCase())
+  }
+
+  const handlePassword = (event) => {
+    setFormPassword(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    
+    console.log(formName, formPassword)
+    //setar estado de loading
+    setFormName('')
+    setFormPassword('')
+    alert('Formulario enviado!')
+    // fetch('heroku.com/yourdev/addProduct',{
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     name: formName,
+    //     password: formPassword
+    //   })
+    // }).then(res => res.json())
+    // .then(json => {
+    //   // fazer o que quiser com o json
+      
+    //   // setar estado de loadin falso
+    // }).catch(error => {
+    //   // setar estado de loadin falso
+    //   if(error.type == 'Non Authenticated'){
+    //     // setar estado true popup nao autenticado
+    //   }
+    //   console.log(error)
+    // })
+
+    event.preventDefault();
+  }
+
   return (
-    <>
     <div className="section__comoFunciona">
       <div className="section__comoFuncionaTitle">
-      <h1>Como funciona?</h1>
-      <p>  Desenvolva sua ideia e o seu site com a gente e aproveite! </p>
-
+        <h1>Como funciona?</h1>
+        <p>  Desenvolva sua ideia e o seu site com a gente e aproveite! </p>
       </div>
       <section className="section__comoFunciona_content">
+        <section class="container">
+          <Box imgUrl={CreativeThinkingUrl} text="Tem uma ideia, serviço ou produto?"/>
+          <Box imgUrl={EscolharOMelhorUrl} text="Tenha uma reunião online com nossa equipe"/>
+          <Box text="Objetivo alcançado ! Aproveite seu site!"/>
+          <Box text="Escolha o melhor plano para o seu negócio!"/>
+          <Box text="Receba entregas do seu site durante a criação"/> 
+        </section>
+        <form onSubmit={handleSubmit}>
+          <label>Nome</label>
+          <input value={formName} onChange={handleName}></input>
+          <label>Senha</label>
+          <input type="password" value={formPassword} onChange={handlePassword}></input>
+          <input type='submit' value="Confirmar"></input>
+        </form>
 
-      <section class="container">
-	
-  <div class="box">
-      <div className="box1">
-      <h1>Tem uma ideia, serviço ou produto?</h1>
-      </div>
-  </div>
-
-  <div class="box">
-  <div className="box2">
-      <h1>Tenha uma reunião online com nossa equipe</h1>
-      </div>
-     
-  </div>
-
-  <div class="box">
-  <div className="box1">
-      <h1>Objetivo alcançado ! Aproveite seu site!</h1>
-      </div>
-  </div>
-
-  <div class="box">
-  <div className="box2">
-      <h1>Escolha o melhor plano para o seu negócio!</h1>
-      </div>
-      
-      
-  </div>
-
-  <div class="box">
-  <div className="box2">
-      <h1>Receba entregas do seu site durante a criação</h1>
-      </div>
-     
-  </div>
-
-  
-       
-</section>
-<div>
-
-</div>
-
-
+        {/* <Acordeao/> */}
       </section>
-    
-
     </div>
-
-    <div className="vector">
-
-    </div>
-    
-
-
-    </>
-   
   );
 }
 
---
+++ src/components/Acordeao.js
@@ -0,0 +1,118 @@
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import './Acordeao.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  expansion: {
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'red',
+    borderRadius: 5,
+  },
+  expansionWithoutBorder: {
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'red',
+    borderBottom: 0,
+    borderRadius: '5px 5px 0px 0px',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '100%',
+    flexShrink: 0,
+    fontWeight: 'bold',
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  exansionDetailsText:{
+    color: 'blue'
+  }
+}));
+
+export default function ControlledExpansionPanels() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <ExpansionPanelSummary
+          className={expanded === 'panel1' ? classes.expansionWithoutBorder : classes.expansion}
+          expandIcon={<ExpandMoreIcon style={{ color: 'black', fontSize: 30 }} />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>1. O que é um negócio Digital e como a Your/Dev pode ajudar?</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className="expasion__details">
+          <Typography className={classes.exansionDetailsText}>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+            maximus est, id dignissim quam.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography className={classes.heading}>Users</Typography>
+
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+            diam eros in elit. Pellentesque convallis laoreet laoreet.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography className={classes.heading}>Advanced settings</Typography>
+
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+            vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography className={classes.heading}>Personal data</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
+            vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+}
