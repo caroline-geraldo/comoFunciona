548d68614d1021605f553c5f100f7a26da542f1a
Caroline geraldo <carolinegeraldo@yahoo.com.br>
2020-05-30 16:10:54 -0300

    finalizada secao como funciona

--- src/App.js
+++ src/App.js
@@ -2,77 +2,37 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import CreativeThinkingUrl from './assets/img/creative-thinking.png'
-import EscolharOMelhorUrl from './assets/img/escolhaOMelhor.jpg'
+
+
+import CreativeThinkingUrl from './assets/img/cardescolha.svg'
+import EscolharOMelhorUrl from './assets/img/cardmeeting.svg'
+import RecebaEntregasUrl from './assets/img/cardwebsite.svg'
+import ObjetivoAlcancado from './assets/img/cardfoguete2.svg'
+
 import Box from './components/Box';
 
 import Acordeao from './components/Acordeao';
 
 function App() {
 
-  const [formName, setFormName] = useState('');
-  const [formPassword, setFormPassword] = useState('');
-
-  const handleName = (event) => {
-    setFormName(event.target.value.toUpperCase())
-  }
-
-  const handlePassword = (event) => {
-    setFormPassword(event.target.value)
-  }
-
-  const handleSubmit = (event) => {
-    
-    console.log(formName, formPassword)
-    //setar estado de loading
-    setFormName('')
-    setFormPassword('')
-    alert('Formulario enviado!')
-    // fetch('heroku.com/yourdev/addProduct',{
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     name: formName,
-    //     password: formPassword
-    //   })
-    // }).then(res => res.json())
-    // .then(json => {
-    //   // fazer o que quiser com o json
-      
-    //   // setar estado de loadin falso
-    // }).catch(error => {
-    //   // setar estado de loadin falso
-    //   if(error.type == 'Non Authenticated'){
-    //     // setar estado true popup nao autenticado
-    //   }
-    //   console.log(error)
-    // })
-
-    event.preventDefault();
-  }
 
   return (
     <div className="section__comoFunciona">
       <div className="section__comoFuncionaTitle">
-        <h1>Como funciona?</h1>
-        <p>  Desenvolva sua ideia e o seu site com a gente e aproveite! </p>
+        {/*<h1>Como funciona?</h1>
+        <p>  Desenvolva sua ideia e o seu site com a gente e aproveite! </p>*/}
       </div>
       <section className="section__comoFunciona_content">
         <section class="container">
-          <Box imgUrl={CreativeThinkingUrl} text="Tem uma ideia, serviço ou produto?"/>
-          <Box imgUrl={EscolharOMelhorUrl} text="Tenha uma reunião online com nossa equipe"/>
-          <Box text="Objetivo alcançado ! Aproveite seu site!"/>
-          <Box text="Escolha o melhor plano para o seu negócio!"/>
-          <Box text="Receba entregas do seu site durante a criação"/> 
+          <Box imgUrl={CreativeThinkingUrl} />
+          <Box imgUrl={EscolharOMelhorUrl} text=""/>
+          <Box imgUrl={RecebaEntregasUrl} text=""/>
+          <Box imgUrl={ObjetivoAlcancado} text="                    "/>
+         {/* <Box text="Receba entregas do seu site durante a criação"/> */}
         </section>
-        <form onSubmit={handleSubmit}>
-          <label>Nome</label>
-          <input value={formName} onChange={handleName}></input>
-          <label>Senha</label>
-          <input type="password" value={formPassword} onChange={handlePassword}></input>
-          <input type='submit' value="Confirmar"></input>
-        </form>
+      
 
-        {/* <Acordeao/> */}
+        
       </section>
     </div>
   );
